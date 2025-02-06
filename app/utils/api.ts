61a7238a5c4959a00960d4ba4c3633a8f4568a9a
Eduardo Jiménez <eduardjim02@gmail.com>
2025-02-06 16:44:45 -0500

    fix para levantar json server

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,8 +1,8 @@
 // utils/api.ts
 export const fetchProducts = async (searchTerm: string) => {
   console.log('searchTerm',searchTerm)
-  const response = await fetch(`http://localhost:3001/products?query=${encodeURIComponent(searchTerm)}`);
-  // const response = await fetch(`http://localhost:3001/products?q=${searchTerm}&_limit=8&_page=${page}`);
+  const response = await fetch(`https://json-server-test-front-vercel.vercel.app/products?query=${encodeURIComponent(searchTerm)}`);
+  // const response = await fetch(`http://localhost:3001/products?query=${encodeURIComponent(searchTerm)}`);
   return response.json();
 };
 
@@ -10,7 +10,8 @@ export const fetchProducts = async (searchTerm: string) => {
 import { Product } from '../types'; // Asegúrate de importar el tipo
 
 export const fetchProduct = async (sku: string): Promise<Product | null> => {
-  const response = await fetch(`http://localhost:3001/products?sku=${sku}`);
+  const response = await fetch(`https://json-server-test-front-vercel.vercel.app/products?sku=${sku}`);
+  // const response = await fetch(`http://localhost:3001/products?sku=${sku}`);
   const data: Product[] = await response.json(); // Asegúrate de que TypeScript sepa que es un array de Product
 
   // Devuelve el primer producto encontrado o null si no hay productos
